02c7bcfd24397969a86418e6f3d5e25c5975f85e
chadstewart <chadrhonanstewart@gmail.com>
2022-10-11 11:56:21 -0500

    Successfully have bot retweet statuses from a particular hashtag.

--- bot.ts
+++ bot.ts
@@ -1,6 +1,8 @@
 import { ETwitterStreamEvent } from "twitter-api-v2";
 import twitterClient from "./config/config";
 
+const TECHISHIRINGTWITTERID = "1392543731866390531"
+
 const initializeBot = async () => {
   
   const stream = await twitterClient.v1.filterStream({
@@ -10,7 +12,12 @@ const initializeBot = async () => {
 
   stream.on(ETwitterStreamEvent.Data, (tweet) => {
     console.log(tweet);
-  })
+
+    if(tweet.user.id_str !== TECHISHIRINGTWITTERID){
+      twitterClient.v1.post("favorites/create.json", { id: tweet.id_str });
+      twitterClient.v1.post(`statuses/retweet/${tweet.id_str}.json`);
+    }
+  });
 
 };
 
