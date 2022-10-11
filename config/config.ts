ddd2e0369d7011fefffee663fa8cf2b3499d11d0
chadstewart <chadrhonanstewart@gmail.com>
2022-10-11 08:46:51 -0500

    Added env variables to client.

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,11 @@
-console.log("Hello config");
+import { TwitterApi } from "twitter-api-v2";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env"});
+
+const twitterClient = new TwitterApi({
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_SECRET_KEY,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET
+});
---
+++ global.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    API_KEY: string;
+    API_SECRET_KEY: string;
+    ACCESS_TOKEN: string;
+    ACCESS_TOKEN_SECRET: string;
+  }
+}
