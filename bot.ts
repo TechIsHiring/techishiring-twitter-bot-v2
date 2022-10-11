d9f0ede36f0a736c1360103fd0fc5fae21e19c37
chadstewart <chadrhonanstewart@gmail.com>
2022-10-11 12:47:52 -0500

    Updated bot to get mute list from Twitter and not tweet anything from anyone that appears on the mute list.

--- bot.ts
+++ bot.ts
@@ -1,23 +1,31 @@
 import { ETwitterStreamEvent } from "twitter-api-v2";
 import twitterClient from "./config/config";
+import { initializeBanList, checkIfBanned } from "./utils/banned/bannedUtils";
 
 const TECHISHIRINGTWITTERID = "1392543731866390531"
 
 const initializeBot = async () => {
-  
+
+  let banList = await initializeBanList();
+
   const stream = await twitterClient.v1.filterStream({
     track: "#TestIsHiring",
     autoConnect: true
   });
 
-  stream.on(ETwitterStreamEvent.Data, (tweet) => {
+  stream.on(ETwitterStreamEvent.Data, async (tweet) => {
     console.log(tweet);
 
-    if(tweet.user.id_str !== TECHISHIRINGTWITTERID){
+    const notTechIsHiringRetweet = tweet.user.id_str !== TECHISHIRINGTWITTERID;
+    const notBanned = await checkIfBanned(tweet, banList);
+
+    if(notBanned && notTechIsHiringRetweet){
       twitterClient.v1.post("favorites/create.json", { id: tweet.id_str });
       twitterClient.v1.post(`statuses/retweet/${tweet.id_str}.json`);
     }
   });
+
+  setInterval(async () => banList = await initializeBanList(), 1800000);
 
 };
 
---
+++ utils/banned/bannedUtils.ts
@@ -0,0 +1,18 @@
+import { TweetV1 } from "twitter-api-v2";
+import twitterClient from "../../config/config";
+
+export const initializeBanList = async () => {
+  
+  const mutedUsersList = (await twitterClient.v1.listMutedUserIds()).ids;
+  const banList = new Set(mutedUsersList);
+  return banList;
+
+};
+
+export const checkIfBanned = (tweet: TweetV1, banList: Set<string>) => {
+  if(tweet.retweeted_status) {
+    return !(banList.has(tweet.user.id_str) || banList.has(tweet.retweeted_status.user.id_str));
+  }
+
+  return !banList.has(tweet.user.id_str);
+}
