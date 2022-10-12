624b26d074fc46dc2eda06ce6f3dcb1d7acad631
chadstewart <chadrhonanstewart@gmail.com>
2022-10-12 08:15:15 -0500

    Forgot to remove setInterval.

--- bot.ts
+++ bot.ts
@@ -26,7 +26,7 @@ const initializeBot = async () => {
     } */
   });
 
-  setInterval(async () => banList = await initializeBanList(), 1800000);
+  //setInterval(async () => banList = await initializeBanList(), 1800000);
 
 };
 
