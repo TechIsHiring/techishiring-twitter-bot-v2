4d831ab35593ec1ddbad5e47ff28554d2504ce85
chadstewart <chadrhonanstewart@gmail.com>
2022-10-13 08:21:47 -0500

    Deployed app on DigitalOcean so making the app actually work.

--- bot.ts
+++ bot.ts
@@ -16,17 +16,17 @@ const initializeBot = async () => {
   stream.on(ETwitterStreamEvent.Data, async (tweet) => {
     console.log(tweet);
 
-    /* const notTechIsHiringRetweet = tweet.user.id_str !== TECHISHIRINGTWITTERID;
+    const notTechIsHiringRetweet = tweet.user.id_str !== TECHISHIRINGTWITTERID;
     const notBanned = await checkIfBanned(tweet, banList);
     const permittedTweet = notBanned && notTechIsHiringRetweet;
 
     if(permittedTweet){
       twitterClient.v1.post("favorites/create.json", { id: tweet.id_str });
       twitterClient.v1.post(`statuses/retweet/${tweet.id_str}.json`);
-    } */
+    }
   });
 
-  //setInterval(async () => banList = await initializeBanList(), 1800000);
+  setInterval(async () => banList = await initializeBanList(), 1800000);
 
 };
 
