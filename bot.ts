241d88c2b111ffeb5b2607252148a7dedc4e4417
chadstewart <chadrhonanstewart@gmail.com>
2022-10-14 13:41:44 -0500

    Adding a console log for when a tweet is seen but is not permitted.

--- bot.ts
+++ bot.ts
@@ -25,10 +25,13 @@ const initializeBot = async () => {
       try {
         await twitterClient.v1.post("favorites/create.json", { id: tweet.id_str });
         await twitterClient.v1.post(`statuses/retweet/${tweet.id_str}.json`);
+        return
       } catch (error) {
         console.log(error);
       }
     }
+
+    console.log("This tweet was not a permitted tweet: " + tweet);
   });
 
   setInterval(async () => banList = await initializeBanList(), 1800000);
